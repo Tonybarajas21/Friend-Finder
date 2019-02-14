11c27507b7706b18f05b5a30f5bd2dca598af114
TonyBee <josebarajas2169@gmail.com>
2019-02-13 21:45:09 -0700

    modules working

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+    {
+      name: 'Bear Grylls',
+      profilePic: 'https://starsunfolded.com/wp-content/uploads/2016/09/Bear-Grylls.jpg',
+      scores: [
+        "5",
+        "5",
+        "4",
+        "1",
+        "3",
+        "5",
+        "3",
+        "4",
+        "3",
+        "1"
+      ]
+    }
+  ]
+  
+  module.exports = friendList;
